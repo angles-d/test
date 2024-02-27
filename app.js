fb3472215a4a6e28442b2d8dc853bb10c7b0ba76
angles-d <zhjh.angela@gmail.com>
2024-02-26 19:53:02 -0500

    update vals

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// Generating content based on the template
-const template = `<article>
-  <img src='data/img/placeholder.png' data-src='data/img/SLUG.jpg' alt='NAME'>
-  <h3>#POS. NAME</h3>
-  <ul>
-  <li><span>Author:</span> <strong>AUTHOR</strong></li>
-  <li><span>Twitter:</span> <a href='https://twitter.com/TWITTER'>@TWITTER</a></li>
-  <li><span>Website:</span> <a href='http://WEBSITE/'>WEBSITE</a></li>
-  <li><span>GitHub:</span> <a href='https://GITHUB'>GITHUB</a></li>
-  <li><span>More:</span> <a href='http://js13kgames.com/entries/SLUG'>js13kgames.com/entries/SLUG</a></li>
-  </ul>
-</article>`;
-let content = "";
-for (let i = 0; i < games.length; i++) {
-  let entry = template
-    .replace(/POS/g, i + 1)
-    .replace(/SLUG/g, games[i].slug)
-    .replace(/NAME/g, games[i].name)
-    .replace(/AUTHOR/g, games[i].author)
-    .replace(/TWITTER/g, games[i].twitter)
-    .replace(/WEBSITE/g, games[i].website)
-    .replace(/GITHUB/g, games[i].github);
-  entry = entry.replace("<a href='http:///'></a>", "-");
-  content += entry;
-}
-document.getElementById("content").innerHTML = content;
-
 // Registering Service Worker
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js");
@@ -43,39 +16,20 @@ button.addEventListener("click", () => {
 
 // Setting up random Notification
 function randomNotification() {
-  const notifTitle = "notif";
-  const notifBody = `Created by.`;
-  const notifImg = `data/img/a-box-invaders.jpg`;
+  const notifTitle =
+    "Takeaways From the Supreme Court Arguments on Social Media Laws";
+  const notifBody = `Laws in Texas and Florida seek to limit social media companies’ ability to moderate content on their platforms and could shape the future of speech on the internet.`;
+  const notifImg = `data/img/sc.webp`;
   const options = {
     body: notifBody,
     icon: notifImg,
   };
-  notif = new Notification(notifTitle);
-  console.log(notif);
-}
-
-// Progressive loading images
-const imagesToLoad = document.querySelectorAll("img[data-src]");
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
+  notif = new Notification(notifTitle, options);
+  notif.onclick = (event) => {
+    event.preventDefault(); // prevent the browser from focusing the Notification's tab
+    window.open(
+      "https://www.nytimes.com/2024/02/26/us/politics/supreme-court-social-media-takeaways.html",
+      "_blank"
+    );
   };
-};
-if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
-    });
-  });
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
-} else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
 }
